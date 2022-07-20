144100346a359a327d7892b5bbe9b19f9d00968b
Albert Nurgaliev <anurgaliev@cloudally.com>
2022-07-20 14:32:47 +0300

    Add files via upload

---
+++ StringExample.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class StringExample {
+
+	/**
+	 * это мой мейн метод - точка входа в программу.
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String myName = "First Last";
+		String firstName= "", lastName = "";
+		//myName.split();
+		//myName.substring(); и myName.indexOf()
+
+		Scanner sc = new Scanner(System.in);
+		String someString = sc.next(); // Albert Nurgaliev
+
+
+		System.out.println(firstName);
+		System.out.println(lastName);
+	}
+}
